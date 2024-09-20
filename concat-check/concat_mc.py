e586cc2fe512cc7a7ebf1e2bdeaa9535fc83006f
el-natano <BartonNa816@northwested.org>
2024-09-20 10:54:44 -0400

    Create concat_mc.py

---
+++ concat-check/concat_mc.py
@@ -0,0 +1,30 @@
+# Nathan Barton
+# September 20, 2024
+# Conatenation Check
+
+
+# Task 1
+
+first_name = 'Nathan'
+
+junk_food = 'pizza'
+
+print('My name is ' + first_name + ' and my favorite junk food is ' + junk_food + '.')
+print(' ')
+
+# Task 2
+
+print('I personally prefer f strings. They feel easier and less complicated.')
+print(' ')
+print('Contatination uses a lot of addition opperators.')
+print(' ')
+print('Sometimes you have to try a few times because of spacing and grammatical errors.')
+print(' ')
+
+
+# Task 3
+
+burger_price = '3.78'
+
+print('A Junior Cheese Burger would cost you ' + '$' + burger_price + ' at Burger King' + '.')
+print(' ')
